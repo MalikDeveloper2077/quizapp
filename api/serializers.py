1396202a736e90e9b6f472fa93cbd2126b8d11e8
MalikDeveloper2077 <nAsad1385@gmail.com>
2020-03-09 21:22:06 +0400

    Added almost the whole functionality (quiz passing)

--- api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from quizapp.models import Comment, Bookmark
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['body']
